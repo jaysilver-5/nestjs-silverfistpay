433a7acd50ba3de165f6c43f8978c64eddf75a82
jaysilver-5 <udojoshua3@gmail.com>
2024-02-17 12:56:15 +0100

    ci: initialize the /checkout route

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
